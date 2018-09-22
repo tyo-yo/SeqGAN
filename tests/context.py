ea8e01174d08afc60e7403ea295886c0280cd0bc
tyo-yo <youtyou0213@gmail.com>
2018-09-23 02:10:37 +0900

    Add tests directory and test for GeneratorPretrainingGenerator

---
+++ tests/context.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import unittest
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from SeqGAN.utils import GeneratorPretrainingGenerator
---
+++ tests/test_GeneratorPretrainingGenerator.py
@@ -0,0 +1,30 @@
+from .context import unittest, os, GeneratorPretrainingGenerator
+# from tests.context import unittest, os, GeneratorPretrainingGenerator
+
+top = os.getcwd()
+
+class TestGeneratorPretrainingGenerator(unittest.TestCase):
+    def sub_test(self, actual, expected, msg=None):
+        with self.subTest(actual=actual, expected=expected):
+            self.assertEqual(actual, expected, msg=msg)
+
+    def test_generator_pretraining_generator(self):
+        gen = GeneratorPretrainingGenerator(
+            os.path.join(top, 'data', 'kokoro_parsed.txt'),
+            B=8,
+            shuffle=False)
+        gen.reset()
+        x, y_true = gen.next()
+
+        expected_text = ['<S>', '私', 'は', 'その', '人', 'を', '常に', '先生', 'と', '呼ん', 'で', 'い', 'た', '。', '</S>']
+        length = len(expected_text)
+        actual_text = [gen.id2word[id] for id in x[0][:length]]
+        self.sub_test(actual_text, expected_text, msg='x text test')
+
+        expected_text = ['だから', 'ここ', 'でも', 'ただ', '先生', 'と', '書く', 'だけ', 'で', '本名', 'は', '打ち明け', 'ない', '。', '</S>']
+        expected_ids = [gen.word2id[word] for word in expected_text]
+        actual_ids = y_true[1][:len(expected_ids)]
+        result = (actual_ids == expected_ids).all()
+        self.assertTrue(result, msg='y_true ids test')
+
+        self.sub_test(gen.len, 4267//8)
