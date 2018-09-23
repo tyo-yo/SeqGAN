e1af0660a8345791ddc9ac6280d69ddfd5d3ea8e
tyo-yo <youtyou0213@gmail.com>
2018-09-23 20:10:55 +0900

    Add DiscriminatorGenerator and Fix comments

--- SeqGAN/utils.py
+++ SeqGAN/utils.py
@@ -113,18 +113,14 @@ class GeneratorPretrainingGenerator(Sequence):
         x, y_true = generator.__getitem__(idx=11)
         print(x[0])
         >>> 8, 10, 6, 3, 2, 0, 0, ..., 0
-        print(y_true[0])
-        >>> 1, 8, 10, 6, 3, 2, 0, ..., 0
+        print(y_true[0][0])
+        >>> 0, 1, 0, 0, 0, 0, 0, ..., 0
 
         id2word = generator.id2word
 
         x_words = [id2word[id] for id in x[0]]
         print(x_words)
         >>> <S> I have a <UNK> </S> <PAD> ... <PAD>
-
-        y_true_words = [id2word[id] for id in y_true[0]]
-        print(y_true_words)
-        >>> I have a <UNK> </S> <PAD> <PAD> ... <PAD>
     '''
     def __init__(self, path, B, T=40, min_count=1, shuffle=True):
         self.PAD = 0
@@ -173,7 +169,8 @@ class GeneratorPretrainingGenerator(Sequence):
         # Returns:
             None: no input is needed for generator pretraining.
             x: numpy.array, shape = (B, max_length)
-            y_true: numpy.array, shape = (B, max_length)
+            y_true: numpy.array, shape = (B, max_length, V)
+                labels with one-hot encoding.
                 max_length is the max length of sequence in the batch.
                 if length smaller than max_length, the data will be padded.
         '''
@@ -235,6 +232,164 @@ class GeneratorPretrainingGenerator(Sequence):
         self.idx = 0
         if self.shuffle:
             self.shuffled_indices = np.arange(self.n_data)
+            random.shuffle(self.shuffled_indices)
+
+    def on_epoch_end(self):
+        self.reset()
+        pass
+
+
+class DiscriminatorGenerator(Sequence):
+    '''
+    Generate generator pretraining data.
+    # Arguments
+        path_pos: str, path to true data
+        path_neg: str, path to generated data
+        B: int, batch size
+        T (optional): int or None, default is None.
+            if int, T is the max length of sequential data.
+        min_count (optional): int, minimum of word frequency for building vocabrary
+        shuffle (optional): bool
+
+    # Params
+        PAD, BOS, EOS, UNK: int, id
+        PAD_TOKEN, UNK_TOKEN, BOS_TOKEN, EOS_TOKEN: str
+        B, min_count: int
+        vocab: Vocab
+        word2id: Vocab.word2id
+        id2word: Vocab.id2word
+        raw_vocab: Vocab.raw_vocab
+        V: the size of vocab
+        n_data: the number of rows of data
+
+    # Examples
+        generator = VAESequenceGenerator('./data/train_x.txt', 32)
+        X, Y = generator.__getitem__(idx=11)
+        print(X[0])
+        >>> 8, 10, 6, 3, 2, 0, 0, ..., 0
+        print(Y)
+        >>> 0, 1, 1, 0, 1, 0, 0, ..., 1
+
+        id2word = generator.id2word
+
+        x_words = [id2word[id] for id in X[0]]
+        print(x_words)
+        >>> I have a <UNK> </S> <PAD> ... <PAD>
+    '''
+    def __init__(self, path_pos, path_neg, B, T=40, min_count=1, shuffle=True):
+        self.PAD = 0
+        self.BOS = 1
+        self.EOS = 2
+        self.UNK = 3
+        self.PAD_TOKEN = '<PAD>'
+        self.UNK_TOKEN = '<UNK>'
+        self.BOS_TOKEN = '<S>'
+        self.EOS_TOKEN = '</S>'
+        self.path_pos = path_pos
+        self.path_neg = path_neg
+        self.B = B
+        self.T = T
+        self.min_count = min_count
+
+        default_dict = {
+            self.PAD_TOKEN: self.PAD,
+            self.BOS_TOKEN: self.BOS,
+            self.EOS_TOKEN: self.EOS,
+            self.UNK_TOKEN: self.UNK,
+        }
+        self.vocab = Vocab(default_dict, self.UNK_TOKEN)
+        sentences = load_data(path_pos)
+        self.vocab.build_vocab(sentences, self.min_count)
+
+        self.word2id = self.vocab.word2id
+        self.id2word = self.vocab.id2word
+        self.raw_vocab = self.vocab.raw_vocab
+        self.V = len(self.vocab.word2id)
+        with open(path_pos, 'r', encoding='utf-8') as f:
+            self.n_data_pos = sum(1 for line in f)
+        with open(path_neg, 'r', encoding='utf-8') as f:
+            self.n_data_neg = sum(1 for line in f)
+        self.n_data = self.n_data_pos + self.n_data_neg
+        self.shuffle = shuffle
+        self.idx = 0
+        self.len = self.__len__()
+        self.reset()
+
+    def __len__(self):
+        return self.n_data // self.B
+
+    def __getitem__(self, idx):
+        '''
+        Get generator pretraining data batch.
+        # Arguments:
+            idx: int, index of batch
+        # Returns:
+            None: no input is needed for generator pretraining.
+            X: numpy.array, shape = (B, max_length)
+            Y: numpy.array, shape = (B, )
+                labels indicate whether sentences are true data or generated data.
+                if true data, y = 1. Else if generated data, y = 0.
+        '''
+        X, Y = [], []
+        start = idx * self.B + 1
+        end = (idx + 1) * self.B + 1
+        max_length = 0
+        for i in range(start, end):
+            if self.shuffle:
+                idx = self.shuffled_indices[i]
+            else:
+                idx = i
+
+            is_pos = 1
+            if idx < 0:
+                is_pos = 0
+                idx = -1 * idx
+            idx = idx - 1   # from 1 origin to 0 origin
+
+            if is_pos == 1:
+                sentence = linecache.getline(self.path_pos, idx) # str
+            elif is_pos == 0:
+                sentence = linecache.getline(self.path_neg, idx) # str
+
+            words = sentence.strip().split()  # list of str
+            ids = sentence_to_ids(self.vocab, words) # list of ids
+
+            x = []
+            x.extend(ids)
+            x.append(self.EOS) # ex. [8, 10, 6, 3, EOS]
+            X.append(x)
+            Y.append(is_pos)
+
+            max_length = max(max_length, len(x))
+
+        if self.T is not None:
+            max_length = self.T
+
+        for i, ids in enumerate(X):
+            X[i] = X[i][:max_length]
+
+        X = [pad_seq(sen, max_length) for sen in X]
+        X = np.array(X, dtype=np.int32)
+
+        return (X, Y)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.idx >= self.len:
+            self.reset()
+            raise StopIteration
+        X, Y = self.__getitem__(self.idx)
+        self.idx += 1
+        return (X, Y)
+
+    def reset(self):
+        self.idx = 0
+        if self.shuffle:
+            pos_indices = np.arange(start=1, stop=self.n_data_pos+1)
+            neg_indices = -1 * np.arange(start=1, stop=self.n_data_neg+1)
+            self.shuffled_indices = np.concatenate([pos_indices, neg_indices])
             random.shuffle(self.shuffled_indices)
 
     def on_epoch_end(self):
