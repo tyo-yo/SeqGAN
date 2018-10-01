47fd03d37d3df3eaad02f8e7a66e25d39e8715f0
tyo-yo <youtyou0213@gmail.com>
2018-10-01 17:40:29 +0900

    Fix bugs in Trainer and Remove unnecessary sentences

--- SeqGAN/train.py
+++ SeqGAN/train.py
@@ -10,38 +10,49 @@ class Trainer(object):
     '''
     Manage training
     '''
-    def __init__(g_B, g_T, g_E, g_H, d_B, d_E, d_filter_sizes, d_num_filters, d_dropout, n_sample):
+    def __init__(self, g_B, g_T, g_E, g_H, d_B, d_E, d_filter_sizes, d_num_filters, d_dropout, n_sample):
         self.g_B, self.g_T, self.g_E, self.g_H = g_B, g_T, g_E, g_H
         self.d_B, self.d_E, self.d_filter_sizes = d_B, d_E, d_filter_sizes
         self.d_num_filters, self.d_dropout = d_num_filters, d_dropout
         self.top = os.getcwd()
-        self.path_pos = os.path.join(top, 'data', 'kokoro_parsed.txt')
+        self.path_pos = os.path.join(self.top, 'data', 'kokoro_parsed.txt')
         self.g_data = GeneratorPretrainingGenerator(
             self.path_pos,
             B=g_B,
             T=g_T,
             min_count=1)
-        self.V = g_data.V
+        self.V = self.g_data.V
         self.agent = Agent(g_B, self.V, g_E, g_H)
         self.g_beta = Agent(g_B, self.V, g_E, g_H)
         self.discriminator = Discriminator(self.V, d_E, d_filter_sizes, d_num_filters, d_dropout)
-        self.env = Environment(discriminator, g_data, g_beta, n_sample=n_sample)
+        self.env = Environment(self.discriminator, self.g_data, self.g_beta, n_sample=n_sample)
 
         self.generator_pre = GeneratorPretraining(self.V, g_E, g_H)
 
-    def pre_train():
+    def pre_train(self, g_epochs=3, d_epochs=1, g_pre_path=None, d_pre_path=None):
+        if g_pre_path is None:
+            self.g_pre_path = os.path.join(self.top, 'data', 'save', 'generator_pre.hdf5')
+        else:
+            self.g_pre_path = g_pre_path
+
+        if d_pre_path is None:
+            self.d_pre_path = os.path.join(self.top, 'data', 'save', 'discriminator_pre.hdf5')
+        else:
+            self.d_pre_path = d_pre_path
+
         g_adam = Adam(lr=0.1)
         self.generator_pre.compile(g_adam, 'categorical_crossentropy')
-        self.generator_pre.summary()
+        print('Generator pre-training')
+
         self.generator_pre.fit_generator(
-            g_data,
+            self.g_data,
             steps_per_epoch=None,
-            epochs=3)
-        self.g_pre_path = os.path.join(self.top, 'data', 'save', 'generator_pre.hdf5')
-        generator_pre.save_weights(self.g_pre_path)
+            epochs=g_epochs)
+        self.generator_pre.save_weights(self.g_pre_path)
         self.agent.generator.load_weights(self.g_pre_path)
+
         self.path_neg = os.path.join(self.top, 'data', 'save', 'generated_sentences.txt')
-        generate_samples(self.agent.generator, self.g_data, 10000, path_neg)
+        generate_samples(self.agent.generator, self.g_data, 10000, self.path_neg)
 
         self.d_data = DiscriminatorGenerator(
             path_pos=self.path_pos,
@@ -51,37 +62,14 @@ class Trainer(object):
 
         d_adam = Adam()
         self.discriminator.compile(d_adam, 'binary_crossentropy')
-        self.discriminator.summary()
+        print('Discriminator pre-training')
+        
         self.discriminator.fit_generator(
-            d_data,
+            self.d_data,
             steps_per_epoch=None,
             epochs=1)
+        self.discriminator.save(self.d_pre_path)
 
-
-g_B, g_E, g_H, g_T = 32, 32, 32, 40
-d_B, d_E = 32, 32
-d_filter_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20] # filter sizes for CNNs
-d_num_filters = [100, 200, 200, 200, 200, 100, 100, 100, 100, 100, 160, 160] # num of filters for CNNs
-n_sample=16
-
-top = os.getcwd()
-path_pos = os.path.join(top, 'data', 'kokoro_parsed.txt')
-g_data = GeneratorPretrainingGenerator(
-    os.path.join(top, 'data', 'kokoro_parsed.txt'),
-    B=g_B,
-    T=g_T,
-    min_count=1)
-V = g_data.V
-agent = Agent(g_B, V, g_E, g_H)
-g_beta = Agent(g_B, V, g_E, g_H)
-discriminator = Discriminator(V, d_E, d_filter_sizes, d_num_filters, 0.1)
-env = Environment(discriminator, g_data, g_beta, n_sample=n_sample)
-
-generator_pre = GeneratorPretraining(V, g_E, g_H)
-
-x, y =d_data.next()
-pred = discriminator.predict(x)
-for i in range(d_B):
-    txt = [g_data.id2word[id] for id in x[i].tolist()]
-    label = y[i]
-    print('{}, {:.3f}: {}'.format(label, pred[i,0], ''.join(txt)))
+    def load_pre_train(self, g_pre_path, d_pre_path):
+        self.agent.generator.load_weights(g_pre_path)
+        self.discriminator.load_weights(d_pre_path)
