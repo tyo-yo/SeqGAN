102c342453ca8e15528427cacb06d2771e893a2f
tyo-yo <youtyou0213@gmail.com>
2018-10-06 15:48:14 +0900

    Update Agent to support Generator chenges

--- SeqGAN/rl.py
+++ SeqGAN/rl.py
@@ -1,5 +1,5 @@
 from SeqGAN.models import Generator, GeneratorPretraining, Discriminator
-from SeqGAN.utils import DiscriminatorGenerator, softmax
+from SeqGAN.utils import DiscriminatorGenerator
 import keras.backend as K
 import numpy as np
 
@@ -8,50 +8,25 @@ class Agent(object):
     On each step, Agent act on state.
     Then Environment return next state, reward, and so on.
     '''
-    def __init__(self, B, V, E, H):
+    def __init__(self, sess, B, V, E, H, lr=1e-3):
         '''
         # Arguments:
+            sess: tf.Session
             B: int, batch_size
             V: int, Vocabrary size
             E: int, Embedding size
             H: int, LSTM hidden size
+        # Optional Arguments:
+            lr: float, learning rate, default is 0.001
         '''
+        self.sess = sess
         self.num_actions = V
         self.B = B
         self.V = V
         self.E = E
         self.H = H
-        self.generator = Generator(V, E, H)
-        self.reset_rnn_state()
-
-    def reset_rnn_state(self):
-        self.h = np.zeros([self.B, self.H])
-        self.c = np.zeros([self.B, self.H])
-
-    def set_rnn_state(self, h, c):
-        self.h = h
-        self.c = c
-
-    def get_rnn_state(self):
-        return self.h, self.c
-
-    def evaluate(self, state, model=None):
-        '''
-        Evaluate next word probability by using observed state (word id).
-        model should be stateful.
-        # Arguments:
-            state: numpy array, dtype=int, shape = (B, 1),
-                state indicates current word.
-        # Returns:
-            probs: numpy array, dtype=float, shape = (B, V),
-                probs are next word probabiliies.
-        '''
-        _model = model if model else self.generator
-        logG, h, c = _model.predict([state, self.h, self.c])    # (B, V)
-        probs = np.exp(logG)
-        self.set_rnn_state(h, c)    # Update state
-
-        return probs
+        sess.lr = 1e-3
+        self.generator = Generator(sess, B, V, E, H, lr)
 
     def act(self, state, epsilon=0, deterministic=False):
         '''
@@ -79,27 +54,16 @@ class Agent(object):
         if np.random.rand() <= epsilon:
             action = np.random.randint(low=0, high=self.num_actions, size=(self.B, 1))
         elif not deterministic:
-            probs = self.evaluate(word)
-            action = self.sampling(probs)
+            probs = self.generator.predict(word)
+            action = self.generator.sampling_word(probs)
         else:
-            probs = self.evaluate(word) # (B, T)
+            probs = self.generator.predict(word) # (B, T)
             action = np.argmax(probs, axis=-1).reshape([-1, 1])
         return action
 
-    def sampling(self, probs):
-        '''
-        # Arguments:
-            probs: numpy array, dtype=float, shape = (B, V),
-        # Returns:
-            action: numpy array, dtype=int, shape = (B, 1)
-        '''
-        assert probs.shape[0] == self.B, 'probs shape should be same as B'
-        action = np.zeros((self.B, 1), dtype=np.int32)
+    def reset(self):
+        self.generator.reset_rnn_state()
 
-        for i in range(self.B):
-            p = probs[i]
-            action[i, 0] = np.random.choice(self.num_actions, p=p)
-        return action
 
 class Environment(object):
     '''
@@ -127,11 +91,17 @@ class Environment(object):
         self.g_beta = g_beta
         self.reset()
 
+    def get_state(self):
+        if self.t == 1:
+            return self._state
+        else:
+            return self._state[:, 1:]   # Exclude BOS
+
     def reset(self):
         self.t = 1
-        self.state = np.zeros([self.B, 1], dtype=np.int32)
-        self.state[:, 0] = self.BOS
-        self.g_beta.reset_rnn_state()
+        self._state = np.zeros([self.B, 1], dtype=np.int32)
+        self._state[:, 0] = self.BOS
+        self.g_beta.reset()
 
     def step(self, action):
         '''
@@ -148,17 +118,17 @@ class Environment(object):
         self.t = self.t + 1
 
         reward = self.Q(action, self.n_sample)
-        is_episode_end = self.t >= self.T
+        is_episode_end = self.t > self.T
 
         self._append_state(action)
-        next_state = self.state
+        next_state = self.get_state()
         info = None
 
         return [next_state, reward, is_episode_end, info]
 
     def render(self, head=1):
         for i in range(head):
-            ids = self.state[i, :]
+            ids = self.get_state()[i]
             words = [self.data_generator.id2word[id] for id in ids.tolist()]
             print(''.join(words))
         print('-' * 80)
@@ -174,7 +144,7 @@ class Environment(object):
             n_sample: int, default is 16, number of samples for Monte Calro Search
 
         # Returns:
-            reward: numpy array, dtype=float, shape = (B, 1), State-Action value
+            reward: numpy array, dtype=float, shape = (B, ), State-Action value
 
         # Requires:
             t, T: used to define time range.
@@ -182,26 +152,27 @@ class Environment(object):
             action: next words, y[t], used for sentence Y[0:t].
             g_beta: Rollout policy.
         '''
-        h, c = self.g_beta.get_rnn_state()
+        h, c = self.g_beta.generator.get_rnn_state()
         reward = np.zeros([self.B, 1])
-        Y_base = self.state    # (B, t-1)
+        if self.t == 2:
+            Y_base = self._state    # Initial case
+        else:
+            Y_base = self.get_state()    # (B, t-1)
 
-        if self.t >= self.T - 1:
+        if self.t >= self.T+1:
             Y = self._append_state(action, state=Y_base)
             return self.discriminator.predict(Y)
 
         # Rollout
         for idx_sample in range(n_sample):
             Y = Y_base
-            self.g_beta.set_rnn_state(h, c)
+            self.g_beta.generator.set_rnn_state(h, c)
             y_t = self.g_beta.act(Y, epsilon=0)
             Y = self._append_state(y_t, state=Y)
-
             for tau in range(self.t+1, self.T):
                 y_tau = self.g_beta.act(Y, epsilon=0)
                 Y = self._append_state(y_tau, state=Y)
-            reward += self.discriminator.predict(Y)
-        reward = np.average(reward, axis=-1)
+            reward += self.discriminator.predict(Y) / n_sample
 
         return reward
 
@@ -211,7 +182,8 @@ class Environment(object):
         # Arguments:
             word: numpy array, dtype=int, shape = (B, 1)
         '''
+        word = word.reshape(-1, 1)
         if state is None:
-            self.state = np.concatenate([self.state, word], axis=-1)
+            self._state = np.concatenate([self._state, word], axis=-1)
         else:
             return np.concatenate([state, word], axis= -1)
